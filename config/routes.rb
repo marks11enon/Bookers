cf65326b24e8153147764507bf5a2b508f308883
marks11enon <97105003+marks11enon@users.noreply.github.com>
2022-01-22 14:25:59 +0000

    [Update] 3-10

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  get 'books/index'
-  get 'books/show'
-  get 'books/edit'
-  get 'homes/top' => 'homes#top'
+  get 'books/new' => 'book#new'
+  post 'books' => 'books#create'
+  get 'top' => 'homes#top'
+  get 'books' => 'books#index'
+  root to: "homes#top"
+  get 'books/:id' => 'books#show', as: 'book'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  patch 'books/:id' => 'books#update' , as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
